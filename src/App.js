3c3847ce45ac9f753813fd922cde1ed39af38fb5
edgar humberto perez martinez <edgar@localhost.localdomain>
2020-05-06 20:51:37 -0500

    redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React from "react";
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
-// items
-import cartItems from "./cart-items";
 // redux stuff
+//store - stores data, think of state
+//reducer - function that used to update store
+// two arguments - state,action
+//state - old state/state before update
+//action - what hapenned/what update
+//return updated or old state
+import {createStore} from 'redux';
+import reducer from './reducer';
+//react redux - provider - wraps app, conect - used in components
+import { Provider } from "react-redux";
+//dispatch method - send actions to the store
+//actions(object) - must have type property - what kind of action
+//dont mutate the state - redux built on inmutability copy 
+
+//store
+const store = createStore(reducer);
+
+
+
 
 function App() {
   // cart setup
-
   return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+    <Provider store={store} >
+      <Navbar cart={store.getState} />
+      <CartContainer  />
+    </Provider>
   );
 }
 
